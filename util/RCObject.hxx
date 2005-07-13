491fbcb9bb4f49b398ef15872748b68df9848f12
greear <greear>
2005-07-13 21:01:43 +0000

    Upgrade to latest xml, fixes build on FC4.  Fix some compiler warnings found by latest gcc. --Ben

--- util/RCObject.hxx
+++ util/RCObject.hxx
@@ -66,7 +66,7 @@ public:
       // Do NOT mess with reference count
       return *this;
    }
-   
+   virtual ~RCObject() { };
 
    virtual int incrementReferenceCount() { count++; return count; }
    virtual int decrementReferenceCount() { count--; return count; }
